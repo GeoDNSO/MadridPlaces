df5618fa1d132ef79eb2e9104bea3b11013bf693
Dani <53879220+GeoDNSO@users.noreply.github.com>
2020-10-27 18:49:28 +0100

    Tutorial de Jupyter

--- Examples/ptweepy.py
+++ Examples/ptweepy.py
@@ -9,11 +9,36 @@ consumer_secret = os.getenv("consumer_secret")
 access_token =os.getenv("access_token")
 access_token_secret = os.getenv("access_token_secret")
 
+#Autentificación
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def main():
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+
+def prueba():
+    print("Iniciando pruebas...")
+    # Get the User object for twitter...
+    user = api.get_user('twitter')
+    print(user.screen_name)
+    print(user.followers_count)
+    for friend in user.friends():
+        print(friend.screen_name)
+
+def prueba2():
+   friends = api.friends()
+   for friend in friends:
+       print(friend.screen_name)
+
+
+main()
+
+prueba()
+
+prueba2()
+
