5fc55f35106eaeaae54516f6f95f33c1115a31fb
Dani <53879220+GeoDNSO@users.noreply.github.com>
2021-02-20 21:25:57 +0100

    lista lugares funcional a lo facil

--- App/app/src/main/java/com/example/App/ui/home/HomeFragment.java
+++ App/app/src/main/java/com/example/App/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.App.ui.home;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -9,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.AttributeSet;
 import android.util.Xml;
@@ -18,13 +21,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.example.App.App;
 import com.example.App.R;
 import com.example.App.SessionManager;
+import com.example.App.models.transfer.TPlace;
+import com.example.App.ui.places_list.PlaceListAdapter;
 import com.example.App.ui.places_list.PlacesList;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -36,6 +46,17 @@ public class HomeFragment extends Fragment {
     private Button btn_logout;
 
     private App app; //global variable
+
+    //Parte de la lista
+    private NestedScrollView nestedScrollView;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private ShimmerFrameLayout shimmerFrameLayout;
+
+    private List<TPlace> placeList = new ArrayList<>();
+    private PlaceListAdapter placeListAdapter;
+
+    private int page = 1, limit = 3;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -55,10 +76,12 @@ public class HomeFragment extends Fragment {
 
         viewAccToUser();
 
+        placeListManagement();
+        /*
         Fragment placeListFragment = new PlacesList();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.home_NestedScrollView, placeListFragment).commit();
-
+        */
         return root;
     }
 
@@ -114,6 +137,76 @@ public class HomeFragment extends Fragment {
         btn_register = root.findViewById(R.id.home_register_button);
         btn_login = root.findViewById(R.id.home_login_button);
         btn_logout = root.findViewById(R.id.home_logout_button);
+
+        //parte de la lista
+        nestedScrollView = root.findViewById(R.id.placesList_ScrollView);
+        recyclerView = root.findViewById(R.id.PlaceList_RecyclerView);
+        progressBar = root.findViewById(R.id.placeList_ProgressBar);
+        shimmerFrameLayout = root.findViewById(R.id.placeList_ShimmerLayout);
+    }
+
+    private void placeListManagement(){
+        placeListAdapter = new PlaceListAdapter(getActivity(), placeList); //getActivity = MainActivity.this
+
+        //Set layout
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));//getActivity() en vez de this
+        recyclerView.setAdapter(placeListAdapter);
+
+        getData();
+
+        //Empezar el efecto de shimmer
+        shimmerFrameLayout.startShimmer();
+
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if(scrollY == v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight()){
+                    //Cuando alacance al ultimo item de la lista
+                    //Incrementea el numero de la pagina
+                    page++;
+                    //Mostrar progress bar
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    //Pedimos más datos
+                    getData();
+                }
+            }
+        });
+    }
+
+    //Simula llamada al servidor
+    private void getData() {
+
+        //NO recogemos mas datos al llegar a la pagina 5
+        if(page >= 5){
+            return ;
+        }
+        //Si la respuesta no es nula, es decir, recibimos mensaje del servidor
+        if(true){
+
+            //Esconder la barra de carga
+            progressBar.setVisibility(View.GONE);
+            //Mostrar el recyclerView
+            recyclerView.setVisibility(View.VISIBLE);
+            //Parar el efecto shimmer
+            shimmerFrameLayout.stopShimmer();
+            //Esconder al frameLayout de shimmer
+            shimmerFrameLayout.setVisibility(View.GONE);
+
+            //Usar la respuesta del servidor para ir creando los lugares de la lista
+            for(int i = 0; i < limit; ++i){
+                float rate = (float) Math.random()*6 + 1;
+                TPlace place = new TPlace("Lugar", "Desc", "IMAGEN DEFAULT", rate);
+                //getString(R.drawable.imagen_lugar_default);
+                //Añadir lugar a la lista
+                placeList.add(place);
+            }
+            placeListAdapter = new PlaceListAdapter(getActivity(), placeList);
+
+            recyclerView.setAdapter(placeListAdapter);
+        }else{
+            //Mostrar mensaje de error o trasladar mensaje de error a la vista
+        }
     }
 
 
