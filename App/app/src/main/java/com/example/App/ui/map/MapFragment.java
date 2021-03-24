0c908171adf92456f1ddc787f53df00062227288
juanesco@ucm.es <34p35a2011>
2021-03-24 18:48:38 +0100

    Mapbox introducido

--- App/app/src/main/java/com/example/App/ui/map/MapFragment.java
+++ App/app/src/main/java/com/example/App/ui/map/MapFragment.java
@@ -2,8 +2,10 @@ package com.example.App.ui.map;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,11 +20,22 @@ import android.widget.TextView;
 import com.example.App.R;
 import com.example.App.models.transfer.TPlace;
 import com.example.App.utilities.AppConstants;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
 
 public class MapFragment extends Fragment {
 
     private MapViewModel mViewModel;
-
+    private MapView mapView;
+    private MapboxMap mapboxMap;
     private View root;
 
     private TPlace place;
@@ -33,13 +46,59 @@ public class MapFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         root = inflater.inflate(R.layout.map_fragment, container, false);
 
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) root.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        place = (TPlace) getArguments().getParcelable(AppConstants.BUNDLE_PLACE_DETAILS);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                // Set map style
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+                // Set the camera's starting position
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(place.getLatitude(), place.getLongitude())) // set the camera's center position
+                        .zoom(15)  // set the camera's zoom level
+                        .tilt(20)  // set the camera's tilt
+                        .padding(100, 100, 100, 100)
+                        .build();
+
+                // Move the camera to that position
+                mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+
+
+        });
         tvMapTest = root.findViewById(R.id.tvMapTest);
 
-        place = (TPlace) getArguments().getParcelable(AppConstants.BUNDLE_PLACE_DETAILS);
+
+
 
         tvMapTest.setText("Lugar --> Latitud: "+ place.getLatitude() + " Longitud: " + place.getLongitude());
 
@@ -48,7 +107,58 @@ public class MapFragment extends Fragment {
         ActionBar actionBar = appCompatActivity.getSupportActionBar();
         actionBar.setTitle(place.getName());
 
+
+
+
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 
     @Override
