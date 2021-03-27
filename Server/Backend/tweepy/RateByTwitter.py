a7e0dbc35a92d1940f34db2ed7411a593b7ef1b2
JwangXu <56585998+JwangXu@users.noreply.github.com>
2021-03-28 00:06:31 +0100

    API twitter terminado

--- Server/Backend/tweepy/RateByTwitter.py
+++ Server/Backend/tweepy/RateByTwitter.py
@@ -1,18 +1,13 @@
-import tweepy
 import os
 import io
-import json
 from dotenv import load_dotenv
+
 import tweepy
 from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-from tweepy import Stream
-import time
-import csv
-import re
-import MySQLdb
-import preprocessor as p
-from textblob import TextBlob
+import MySQLdb #Conexión a la BD
+import preprocessor as p #Preprocesa el texto del tweet
+from textblob import TextBlob #Realiza el análisis del sentimiento
+from google_trans_new import google_translator #Traduce el texto para el análisis en inglés
 
 ##############          Este script se ejecutará semanalmente todos los Lunes          ##############
 ############## para actualizar las opiniones de twitter acerca de los lugares de la BD ##############
@@ -23,13 +18,13 @@ def classifyPolarity(polarity):
     #Vamos a clasificarlo del 1 al 5, siendo 1 muy negativo y 5 muy positivo
     if (polarity == 0):  # 3
         rate = 3
-    elif (polarity > 0 and polarity <= 0.6): #4
+    elif (polarity > 0 and polarity <= 0.5): #4
         rate = 4
-    elif (polarity > 0.6 and polarity <= 1): #5
+    elif (polarity > 0.5 and polarity <= 1): #5
         rate = 5
-    elif (polarity > -0.6 and polarity <= 0): #2
+    elif (polarity > -0.5 and polarity <= 0): #2
         rate = 2
-    elif (polarity > -1 and polarity <= -0.6): #1
+    elif (polarity > -1 and polarity <= -0.5): #1
         rate = 1
     return rate
 
@@ -56,33 +51,44 @@ auth.set_access_token(access_token, access_token_secret)
 #Usamos la variable api para utilizar distintas funcionalidades
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-#Recogemos de la BD los nombres de todos los lugares
+#Inicializamos la API de Google traductor
+translator = google_translator()
 
+#Recogemos de la BD los nombres de todos los lugares
 cursor.execute( "SELECT name FROM location") # --> ~5000 lugares
 locations = cursor.fetchall()
+
 for location in locations:
 	try:
-		tweets = tweepy.Cursor(api.search, q=location[0]).items(5) #Generamos los últimos 5 tweets acerca del lugar (parametro modificable)
+		tweets = tweepy.Cursor(api.search, q=location[0], tweet_mode='extended').items(7) #Generamos los últimos 5 tweets acerca del lugar (parametro modificable)
 		listRates = []
-		avgRate = 0
+		avgRate = 0 #Si no hay tweets, avgRate = 0
 		for tweet in tweets:
-		    cleanedTweet = p.clean(tweet.text) #Preprocesa el texto, quita los #, urls y emojis.
+		    cleanedTweet = p.clean(tweet.full_text) #Preprocesa el texto, quita los #, urls y emojis.
+
+		    translation = cleanedTweet
+
 		    if(len(cleanedTweet) > 3): #Para hacer el análisis como mínimo es necesario 3 letras
-		        t = TextBlob(cleanedTweet).detect_language()
-		        analyzed = TextBlob(cleanedTweet)
-		        if(t  == 'es'):
-		            enAnalyzed = analyzed.translate(to='en') #TextBlob no analiza frases en español, por lo que hay que traducirlo. Se perderá un poco de precisión
-		        #print(enAnalyzed) # Comprobar la traducción
+		    	try:
+		    		lang = translator.detect(cleanedTweet)
+		    		if(lang  == 'es'):
+		    			translation = translator.translate(cleanedTweet) #TextBlob no analiza frases en español, por lo que hay que traducirlo. Se perderá un poco de precisión
+		    	except Exception as e:
+		    		print("Ha habido un error:", repr(e))
+		    		pass
+		    	analyzed = TextBlob(translation)
+		        #print(analyzed) # Comprobar la traducción
 		        #print(analyzed.tags) # --> Se puede ver el análisis sintáctico de la frase
-		        #print(enAnalyzed.sentiment)
-		        rate = classifyPolarity(enAnalyzed.sentiment.polarity)
-		        listRates.append(rate)
+		        #print(analyzed.sentiment)
+		    	rate = classifyPolarity(analyzed.sentiment.polarity)
+		    	listRates.append(rate)
+
 		if(len(listRates) != 0):    
 			avgRate = sum(listRates) / len(listRates)
 		print(location[0], avgRate)
 		cursor.execute("INSERT IGNORE INTO twitter_ratings (location, twitterRate) VALUES (%s, %s)", [location, avgRate])
 		mydb.commit()
 	except tweepy.error.TweepError:
-        raise
+		raise
 
 print("Finalizado")
