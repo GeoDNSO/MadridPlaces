6e426dd6db2204692062ca1ab224db50dfa7d3c0
Juan Antonio Escobar de los Angeles <33598927+juanescobar98@users.noreply.github.com>
2021-04-13 18:59:15 +0200

    Mapbox development (#26)
    * Mapbox introducido
    
    * added marker
    
    * Added UserLocation code (failure)
    
    * Correccóon de cambios
    
    * Mapbox with activity
    
    * Mapa y localizacion implementados
    
    * Location mapbox with pulse
    
    * introducing routes
    
    * Rutas hechas sin navegacion
    
    Co-authored-by: juanesco@ucm.es <34p35a2011>
    Co-authored-by: JwangXu <56585998+JwangXu@users.noreply.github.com>

--- App/app/src/main/java/com/example/App/ui/map/MapFragment.java
+++ App/app/src/main/java/com/example/App/ui/map/MapFragment.java
@@ -2,11 +2,16 @@ package com.example.App.ui.map;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,10 +22,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.App.R;
 import com.example.App.models.transfer.TPlace;
 import com.example.App.utilities.AppConstants;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
@@ -28,6 +36,13 @@ import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.OnCameraTrackingChangedListener;
+import com.mapbox.mapboxsdk.location.OnLocationClickListener;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
@@ -44,7 +59,7 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnLocationClickListener, OnCameraTrackingChangedListener, PermissionsListener {
 
     private static final String SOURCE_ID = "SOURCE_ID";
     private static final String ICON_ID = "ICON_ID";
@@ -53,6 +68,9 @@ public class MapFragment extends Fragment {
     private MapView mapView;
     private MapboxMap mapboxMap;
     private View root;
+    private LocationComponent locationComponent;
+    private Boolean isInTrackingMode;
+    private PermissionsManager permissionsManager;
 
     private TPlace place;
     private TextView tvMapTest;
@@ -70,7 +88,9 @@ public class MapFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    public void setMapBox(MapboxMap mapboxMap){
+        this.mapboxMap = mapboxMap;
+    }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -84,10 +104,11 @@ public class MapFragment extends Fragment {
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
-
+                setMapBox(mapboxMap);
                 List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
                 symbolLayerIconFeatureList.add(Feature.fromGeometry(Point.fromLngLat(place.getLongitude(), place.getLatitude())));
                 // Set map style
+                /*
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
@@ -95,11 +116,15 @@ public class MapFragment extends Fragment {
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
 
+                        enableLocationComponent(style);
                     }
                 });
+                 */
+                Bitmap bitmap = BitmapFactory.decodeResource(
+                        getActivity().getResources(), R.drawable.mapbox_marker_icon_default);
                 mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
-                                .withImage(ICON_ID, BitmapFactory.decodeResource(
-                                        getActivity().getResources(), R.drawable.mapbox_marker_icon_default))
+                                .withImage(ICON_ID, bitmap)
+
                                 .withSource(new GeoJsonSource(SOURCE_ID, FeatureCollection.fromFeatures(symbolLayerIconFeatureList)))
                                 .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
                                         .withProperties(
@@ -112,6 +137,7 @@ public class MapFragment extends Fragment {
                             @Override
                             public void onStyleLoaded(@NonNull Style style) {
 
+                                enableLocationComponent(style);
                             }
                         }
                 );
@@ -127,21 +153,18 @@ public class MapFragment extends Fragment {
                 // Move the camera to that position
                 mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+
             }
 
 
-
         });
-
-
 
 
         tvMapTest = root.findViewById(R.id.tvMapTest);
 
 
 
-
-        tvMapTest.setText("Lugar --> Latitud: "+ place.getLatitude() + " Longitud: " + place.getLongitude());
+        tvMapTest.setText("Lugar --> Latitud: " + place.getLatitude() + " Longitud: " + place.getLongitude());
 
         //Poner el nombre del lugar en la toolbar
         AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
@@ -149,12 +172,81 @@ public class MapFragment extends Fragment {
         actionBar.setTitle(place.getName());
 
 
-
-
         return root;
     }
 
-    
+    private void enableLocationComponent(Style style) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(root.getContext())) {
+
+// Create and customize the LocationComponent's options
+            LocationComponentOptions customLocationComponentOptions = LocationComponentOptions.builder(root.getContext())
+                    .elevation(5)
+                    .layerBelow(LAYER_ID)
+                    .accuracyAlpha(.20f)
+                    .accuracyColor(Color.CYAN)
+                    .build();
+
+// Get an instance of the component
+            locationComponent = mapboxMap.getLocationComponent();
+
+            LocationComponentActivationOptions locationComponentActivationOptions =
+                    LocationComponentActivationOptions.builder(root.getContext(), style)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .build();
+
+// Activate with options
+            locationComponent.activateLocationComponent(locationComponentActivationOptions);
+
+// Enable to make component visible
+            if (ActivityCompat.checkSelfPermission(root.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(root.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationComponent.setLocationComponentEnabled(true);
+
+// Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+// Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+// Add the location icon click listener
+            locationComponent.addOnLocationClickListener(this);
+
+// Add the camera tracking listener. Fires if the map camera is manually moved.
+            locationComponent.addOnCameraTrackingChangedListener(this);
+
+            root.findViewById(R.id.back_to_camera_tracking_mode).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!isInTrackingMode) {
+                        isInTrackingMode = true;
+                        locationComponent.setCameraMode(CameraMode.TRACKING);
+                        locationComponent.zoomWhileTracking(16f);
+                        Toast.makeText(root.getContext(), getString(R.string.tracking_enabled),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(root.getContext(), getString(R.string.tracking_already_enabled),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(getActivity());
+        }
+    }
+
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -210,4 +302,32 @@ public class MapFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onLocationComponentClick() {
+        if (locationComponent.getLastKnownLocation() != null) {
+            Toast.makeText(root.getContext(), String.format(getString(R.string.current_location),
+                    locationComponent.getLastKnownLocation().getLatitude(),
+                    locationComponent.getLastKnownLocation().getLongitude()), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onCameraTrackingDismissed() {
+        isInTrackingMode = false;
+    }
+
+    @Override
+    public void onCameraTrackingChanged(int currentMode) {
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
 }
