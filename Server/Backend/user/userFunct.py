1dbf76abe52295d17ffd509055abdf713ccfb08a
JwangXu <56585998+JwangXu@users.noreply.github.com>
2021-04-29 14:03:17 +0200

    Sistema de Amigos en backend

--- Server/Backend/user/userFunct.py
+++ Server/Backend/user/userFunct.py
@@ -78,6 +78,14 @@ def jsonifiedList2(usuario):
     rol=usuario.rol,
     profile_image=usuario.profile_image) #Se devuelve en binario
 
+def friendData(user):
+  obj = {
+  "nickname":user.nickname,
+  "name":user.name,
+  "surname":user.surname,
+  "profile_image": user.profile_image
+  }
+  return obj
 def decode64Img(codedImg): #i para enumerar todas las imágenes
   image = base64.b64decode(str(codedImg))       
   img = Image.open(io.BytesIO(image))
